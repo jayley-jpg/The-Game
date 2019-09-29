aa3a1a5f84c453c6dfa0316121150ad082696851
jayley-jpg <54978194+jayley-jpg@users.noreply.github.com>
2019-09-29 21:21:00 +0200

    Add files via upload

---
+++ kaas.js
@@ -0,0 +1,104 @@
+  alert("Welkom bij de quizzz")
+var name= prompt( "wat is uw naam")
+  alert("welkom " + name + ", Leuk dat je hier bent \nMijn naam is Jayley Troenokarso\nDit is een quiz dat over mij gaat.\nals je alle vragen goed heb dan win je 10 euro\nVeel plezier!");
+  alert("We gaan nu beginnen")
+  alert("Veel succes!")
+age = prompt(" hoe oud ben ik?\n\n(vul 1 in voor)=16 jaar\n(vul 2 in voor)=17jaar\n(vul 3 in voor)=18 jaar");
+
+if (age==1){
+  alert("je antwoord is juist!");
+  alert("we gaan door naar de volgende vraag")
+
+  woonplaats = prompt("In welke stad woon ik?\n(vul 1 in voor )=Dordrecht\n(vul2 in voor)=zwijndrecht\n(vul 3 in voor)=Papendrecht")
+  if (woonplaats==1){
+    alert("je antwoord is juist")
+    alert("we gaan door naar de volgende vraag")
+
+    niveau = prompt("welke niveau deed ik op het middelbare school?\n(vul 1 in voor)=Havo\n(vul 2 in voor)=vmbo-tl\n(vul 3 in voor)=vmbo-kb");
+
+    if(niveau==2){
+      alert ("je antwoord is juist");
+      alert("we gaan door naar de volgende vraag")
+
+   game = prompt ("wat is mijn favoriete game\n(vul 1 in voor)=fifa 20\n(vul 2 in voor)=gta 5\n(vul 3 in voor)=fortnite");
+
+      if(game==1){
+        alert ("je antwoord is juist");
+        alert("we gaan door naar de volgende vraag")
+
+    sport = prompt ("op  welke sport zat ik?\n(vul 1 in voor)=voetbal\n(vul 2 in voor)=volleybal\n(vul 3 in voor)=basketball");
+
+        if(sport==1){
+          alert ("je antwoord is juist");
+
+    merk = prompt ("Wat is mijn favoriete (sport)merk?\n(vul 1 in voor)=adidas\n(vul 2 in voor)=puma\n(vul 3 in voor)=nike");
+
+        if(merk==3){
+          alert ("je antwoord is juist");
+          alert ("cool je hebt de quiz voltooid. Op  basis van deze quiz gaan we een verhaal spelen met de goede antwoorden van de quiz.\nAls je dat ook goed hebt dan heb je gewonnen! ")
+
+    verhaal1 = prompt ("ik ga naar huis wat is het eerste wat ik ga doen?\n(vul 1 in voor)= ik ga op de bank en ik ga tv kijken.\n(vul 2 in voor)Ik ga gelijk naar boven en ik ga fifa spelen.");
+        if(verhaal1 == 2){
+          alert ("je antwoord is juist");
+
+    verhaal2 = prompt ("ik ben klaar met gamen ik ga een sport spelen wat ga ik doen?\n(vul 1 in voor)=Ik ga voetballen\n(vul 2 in voor)=ik ga volleyballen");
+        if(verhaal2 ==1){
+          alert ("je antwoord is juist");
+
+    verhaal3 = prompt ("ik ben aan het voetballen maar welke merk (sport)schoenen gebruik ik\n(vul1 in voor)=nike\n(vul2 in voor)=adidas  ");
+        if(verhaal3 ==1){
+        alert ("je antwoord is juist");
+        alert ("nog 1 vraag en je hebt gewonnen")
+
+
+    verhaal4 = prompt ("wat is mijn favoriete voetbalteam?\n(kies 1 voor)=ajax\n(vul 2 in voor)=psv\n(vul 3 in voor)=feyenoord");
+      if(verhaal4==3){
+      alert ("je antwoord is juist");
+      alert ("gefeliciteerd je heb 10 euro gewonnen!")
+
+
+
+
+          }
+
+
+          }
+
+         }
+
+        }
+
+       }
+
+        }
+        else{
+          alert("sorry je antwoord is fout begin opnieuw");
+          alert("game over");
+          window.location=window.location
+        }
+      }
+      else{
+        alert("sorry je antwoord is fout begin opnieuw");
+        alert("game over");
+        window.location=window.location
+      }
+    }
+    else{
+      alert("sorry je antwoord is fout begin opnieuw");
+      alert("game over");
+      window.location=window.location
+    }
+  }
+  else{
+    alert("sorry je antwoord is fout begin opnieuw");
+    alert("game over");
+    window.location=window.location
+  }
+}
+else{
+  alert("sorry je antwoord is fout begin opnieuw");
+  alert("game over");
+  window.location=window.location
+}
+
+    // begin hier je JavaScript commandos
